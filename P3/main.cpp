374ea744307d86ce442d6b06c8399958eb8abec3
Chloe Coyle <chloe1c@outlook.com>
2022-11-23 19:57:54 +0000

    New project

---
+++ P3/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int num;
+    std::cout << "What is your favourite number? ";
+    std::cin >> num;
+    std::cout << "Amazing that is my favourite number too" << std::endl;
+
+    return 0;
+}
