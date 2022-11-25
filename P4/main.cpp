fcf825d102027232f85972a4fc486e899f749e3d
Chloe Coyle <99185847+ccoyle1@users.noreply.github.com>
2022-11-25 08:24:28 +0000

    Project 4: Print variable to output inside string

---
+++ P4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int num;
+    cout << "What is your favourite number? ";
+    cin >> num;
+    cout << "Amazing that is my favourite number too" << endl;
+    cout << "No really " << num << " is my favourite number!" << endl;
+    
+    return 0;
+}
